c82c1239d830348b9b7b7f7360a63ed38b5791e4
Wojtek <wojtek.kajstura@gmail.com>
2020-12-29 22:27:25 +0100

    Implemented boss entity

---
+++ ProjectManagement/Domain/Entities/Boss.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Base;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class Boss : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<ProjectManager> ProjectManagers { get; set; }
+    }
+}
