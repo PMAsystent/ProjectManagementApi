a939f4f3ff75c2519ebb2a186e1eca26b42fb6f4
Wojtek <wojtek.kajstura@gmail.com>
2020-12-30 00:12:35 +0100

    Implemented oversee model

---
+++ ProjectManagement/Domain/Entities/Oversee.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Base;
+using System;
+
+namespace Domain.Entities
+{
+    public class Oversee : AuditableEntity
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int TaskId { get; set; }
+        public Task Task { get; set; }
+        public ProjectManager ProjectManager { get; set; }
+    }
+}
