80213c3a1d8add18643a570d4eec403544ee4724
Wojtek <wojtek.kajstura@gmail.com>
2020-12-29 22:28:08 +0100

    Implemented project manager entity

---
+++ ProjectManagement/Domain/Entities/ProjectManager.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Base;
+
+namespace Domain.Entities
+{
+    public class ProjectManager : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Boss Boss { get; set; }
+        public Oversee Oversee { get; set; }
+        public Assign Assign { get; set; }
+    }
+}
