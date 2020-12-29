812735f98470ac63d1bda928e9e689ee819d414b
Wojtek <wojtek.kajstura@gmail.com>
2020-12-29 22:26:53 +0100

    Added inheritance of AuditableEntity to existing entities

--- ProjectManagement/Domain/Entities/Task.cs
+++ ProjectManagement/Domain/Entities/Task.cs
@@ -1,22 +1,20 @@
-﻿using Domain.Enums;
+﻿using Domain.Base;
+using Domain.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Task
+    public class Task : AuditableEntity
     {
         public int Id { get; set; }
+        public string Description { get; set; }
         public Priority Priority { get; set; }
         public Status Status { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime TargetDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Description { get; set; }
         public bool IsActive { get; set; }
+
         public int? StepId { get; set; }
         public Step Step { get; set; }
     }
