6b2820e2c8d6a6b37d64cb8cbbf00bd82ac22455
Wojtek <wojtek.kajstura@gmail.com>
2020-12-30 00:12:21 +0100

    Implemented assign model

---
+++ ProjectManagement/Domain/Entities/Assign.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Base;
+using System;
+
+namespace Domain.Entities
+{
+    public class Assign : AuditableEntity
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Task Task { get; set; }
+        public ProjectManager ProjectManager { get; set; }
+
+        // Konrad
+        // public Role Role { get; set; }
+        // public Employee Employee { get; set; }
+    }
+}
