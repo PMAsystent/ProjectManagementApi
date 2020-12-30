b6f674b5d5c064bf03219eb2052efa9b0ea71b29
Wojtek <wojtek.kajstura@gmail.com>
2020-12-30 16:31:30 +0100

    Created seed

--- ProjectManagement/Infrastructure/Persistance/DatabaseContext/ApplicationDbContextSeed.cs
+++ ProjectManagement/Infrastructure/Persistance/DatabaseContext/ApplicationDbContextSeed.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Task = System.Threading.Tasks.Task;
 
 namespace Infrastructure.Persistance.DatabaseContext
 {
@@ -10,10 +14,55 @@ namespace Infrastructure.Persistance.DatabaseContext
             // Seed, if necessary
             if (!context.Projects.Any())
             {
-                context.Projects.Add(new Domain.Entities.Project
+                context.Projects.Add(new()
                 {
-                    Description = "Strona",
-                    IsActive = true
+                    Description = "Test project",
+                    StartDate = DateTime.UtcNow,
+                    EndDate = DateTime.UtcNow.AddMonths(1),
+                    TargetDate = DateTime.UtcNow.AddMonths(1).AddDays(7),
+                    IsActive = true,
+                    Steps = new List<Step>()
+                    {
+                        new()
+                        {
+                            Description = "First step",
+                            StartDate = DateTime.UtcNow,
+                            EndDate = DateTime.UtcNow.AddDays(14),
+                            TargetDate = DateTime.UtcNow.AddDays(14),
+                            IsActive = true,
+                            Tasks = new List<Domain.Entities.Task>()
+                            {
+                                new()
+                                {
+                                    Description = "Task#1",
+                                    Priority = Priority.Level0,
+                                    Status = Status.Start,
+                                    StartDate = DateTime.UtcNow,
+                                    EndDate = DateTime.UtcNow.AddDays(14),
+                                    TargetDate = DateTime.UtcNow.AddDays(14),
+                                    Oversee = new ()
+                                    {
+                                        StartDate = DateTime.UtcNow,
+                                        EndDate = DateTime.UtcNow.AddDays(14),
+                                        ProjectManager = new ()
+                                        {
+                                            Name = "Jan",
+                                            Surname = "Kowalski",
+                                            PhoneNumber = "123123123",
+                                            Email = "jan@kowalski.com",
+                                            Boss = new ()
+                                            {
+                                                Name = "Andrzej",
+                                                Surname = "Nowak",
+                                                PhoneNumber = "123123123",
+                                                Email = "andrzej@nowak.com",
+                                            }
+                                        }
+                                    }
+                                },
+                            }
+                        },
+                    }
                 });
                 await context.SaveChangesAsync();
             }
