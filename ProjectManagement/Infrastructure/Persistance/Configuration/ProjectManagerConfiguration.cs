6db146a38bc5a911846a09eeac1f8363b2696534
Wojtek <wojtek.kajstura@gmail.com>
2020-12-30 16:30:29 +0100

    Implemented relation of PM with Assign and Oversee

---
+++ ProjectManagement/Infrastructure/Persistance/Configuration/ProjectManagerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class ProjectManagerConfiguration : IEntityTypeConfiguration<ProjectManager>
+    {
+        public void Configure(EntityTypeBuilder<ProjectManager> builder)
+        {
+            builder
+                .HasMany(p => p.Oversee)
+                .WithOne(o => o.ProjectManager)
+                .HasForeignKey(o => o.ProjectManagerId);
+
+            builder
+                .HasMany(p => p.Assign)
+                .WithOne(a => a.ProjectManager)
+                .HasForeignKey(a => a.ProjectManagerId);
+        }
+    }
+}
