82772e18c580b815db3953b4277255693d7d9b21
Wojtek <wojtek.kajstura@gmail.com>
2020-12-30 16:30:00 +0100

    Implemented relation of Boss and PM

---
+++ ProjectManagement/Infrastructure/Persistance/Configuration/BossConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class BossConfiguration : IEntityTypeConfiguration<Boss>
+    {
+        public void Configure(EntityTypeBuilder<Boss> builder)
+        {
+            builder
+                .HasMany(b => b.ProjectManagers)
+                .WithOne(p => p.Boss)
+                .HasForeignKey(p => p.BossId);
+        }
+    }
+}
