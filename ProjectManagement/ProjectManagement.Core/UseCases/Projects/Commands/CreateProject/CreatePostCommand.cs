40f803a6892ab417e013b76795570f08dc6d5e69
Wojtek <wojtek.kajstura@gmail.com>
2021-03-06 13:31:03 +0100

    Implemented create post command

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/CreateProject/CreatePostCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace ProjectManagement.Core.UseCases.Projects.Commands.CreateProject
+{
+    public class CreatePostCommand : IRequest<int>
+    {
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime TargetDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
