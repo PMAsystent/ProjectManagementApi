d8c2890793ff810744ea0955d0b52126ad170680
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-06 14:48:43 +0100

    Implemented prototype version of create post command validator

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/CreateProject/CreatePostCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+
+namespace ProjectManagement.Core.UseCases.Projects.Commands.CreateProject
+{
+    public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatePostCommandValidator()
+        {
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.Description).MaximumLength(100);
+            //RuleFor(c => c.StartDate)
+            //    .Must(BeAValidDate).WithMessage("Start date is required");
+            //RuleFor(c => c.TargetDate)
+            //    .Must(BeAValidDate).WithMessage("Target date is required");
+            //RuleFor(c => c.EndDate)
+            //    .Must(BeAValidDate).WithMessage("End date is required");
+        }
+
+        private bool BeAValidDate(DateTime date)
+        {
+            return !date.Equals(default(DateTime));
+        }
+    }
+}
