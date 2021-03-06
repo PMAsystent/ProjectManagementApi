f5530a98bfd6d38ca69482a5eb2c5fac8c5bbb86
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-06 15:48:42 +0100

    Added mapping for CreatePostCommand to Project

--- ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/CreateProject/CreatePostCommand.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/CreateProject/CreatePostCommand.cs
@@ -1,9 +1,11 @@
-﻿using MediatR;
+﻿using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Mappings;
 using System;
 
 namespace ProjectManagement.Core.UseCases.Projects.Commands.CreateProject
 {
-    public class CreatePostCommand : IRequest<CreatePostCommandResponse>
+    public class CreatePostCommand : IRequest<CreatePostCommandResponse>, IMapFrom<Project>
     {
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
@@ -11,5 +13,10 @@ namespace ProjectManagement.Core.UseCases.Projects.Commands.CreateProject
         public DateTime EndDate { get; set; }
         public bool IsActive { get; set; }
         public int CustomerId { get; set; }
+
+        public void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<CreatePostCommand, Project>();
+        }
     }
 }
