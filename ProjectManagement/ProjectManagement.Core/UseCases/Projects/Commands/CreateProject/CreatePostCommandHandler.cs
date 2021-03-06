6ab365598e351af9c8b7d4316de37c627bf8031e
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-06 15:49:02 +0100

    Implemented create post command handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/CreateProject/CreatePostCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Core.UseCases.Projects.Commands.CreateProject
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, CreatePostCommandResponse>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreatePostCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatePostCommandResponse> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreatePostCommandValidator();
+            var validatorResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validatorResult.IsValid)
+            {
+                return new CreatePostCommandResponse(validatorResult);
+            }
+
+            var project = _mapper.Map<Project>(request);
+            await _context.Projects.AddAsync(project, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            Console.WriteLine(project.Id);
+            return new(project.Id);
+        }
+    }
+}
