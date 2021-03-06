d890ebbd926ff8e62dec6b4cf7288f6e998f5323
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-06 14:27:18 +0100

    Implemented create post command response

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/CreateProject/CreatePostCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+
+namespace ProjectManagement.Core.UseCases.Projects.Commands.CreateProject
+{
+    public class CreatePostCommandResponse : BaseResponse
+    {
+        public int? ProjectId { get; set; }
+
+        public CreatePostCommandResponse() : base() { }
+        public CreatePostCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public CreatePostCommandResponse(string message) : base(message) { }
+        public CreatePostCommandResponse(string message, bool success) : base(message, success) { }
+        public CreatePostCommandResponse(int projectId) => ProjectId = projectId;
+    }
+}
