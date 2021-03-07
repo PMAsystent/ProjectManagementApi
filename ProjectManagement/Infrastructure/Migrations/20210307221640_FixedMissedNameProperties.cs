be0e166144e37224fc08225af555855dc9477a4a
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-07 23:19:08 +0100

    Added migration after fix missed name properties

---
+++ ProjectManagement/Infrastructure/Migrations/20210307221640_FixedMissedNameProperties.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class FixedMissedNameProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Steps",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Steps");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Projects");
+        }
+    }
+}
