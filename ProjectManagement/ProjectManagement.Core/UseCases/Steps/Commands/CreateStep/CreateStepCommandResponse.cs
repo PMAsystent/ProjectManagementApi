17624236c92540e8ecb206235a76dd96c4e66f3c
Konrad <konradlubera@hotmail.com>
2021-03-08 19:13:33 +0100

    Added creat step command files

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Steps/Commands/CreateStep/CreateStepCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+
+namespace ProjectManagement.Core.UseCases.Steps.Commands.CreateStep
+{
+    public class CreateStepCommandResponse : BaseResponse
+    {
+        public Step Step { get; set; }
+        
+        public CreateStepCommandResponse() : base() {}
+        public CreateStepCommandResponse(ValidationResult validationResult) : base(validationResult) {}
+        public CreateStepCommandResponse(string message) : base(message){}
+        public CreateStepCommandResponse(string message, bool success) : base(message, success) {}
+        public CreateStepCommandResponse(Step step) => Step = step;
+    }
+}
