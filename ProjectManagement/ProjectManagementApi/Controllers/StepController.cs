3511b0802f316d88ea8edee1af371fdb2a290edb
Konrad <konradlubera@hotmail.com>
2021-03-08 01:26:18 +0100

    Added step controller

--- ProjectManagement/ProjectManagementApi/Controllers/StepController.cs
+++ ProjectManagement/ProjectManagementApi/Controllers/StepController.cs
@@ -1,7 +1,15 @@
-﻿namespace ProjectManagementApi.Controllers
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Steps.Queries.GetSteps;
+
+namespace ProjectManagementApi.Controllers
 {
-    public class StepController
+    public class StepController : ApiControllerBase
     {
-        
+        [HttpGet]
+        public async Task<ActionResult<StepVm>> GetAllSteps()
+        {
+            return await Mediator.Send(new GetStepsQuery());
+        }
     }
 }
