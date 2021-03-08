fb2aa36787c56de1babbc87365318361c47a81a2
Konrad <konradlubera@hotmail.com>
2021-03-08 19:14:39 +0100

    Added http post addStep to controller

--- ProjectManagement/ProjectManagementApi/Controllers/StepController.cs
+++ ProjectManagement/ProjectManagementApi/Controllers/StepController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Steps.Commands.CreateStep;
 using ProjectManagement.Core.UseCases.Steps.Queries.GetSteps;
 
 namespace ProjectManagementApi.Controllers
@@ -10,6 +12,13 @@ namespace ProjectManagementApi.Controllers
         public async Task<ActionResult<StepVm>> GetAllSteps()
         {
             return await Mediator.Send(new GetStepsQuery());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Step>> AddStep([FromBody] CreateStepCommand createStepCommand)
+        {
+            var result = await Mediator.Send(createStepCommand);
+            return Ok(result.Step);
         }
     }
 }
