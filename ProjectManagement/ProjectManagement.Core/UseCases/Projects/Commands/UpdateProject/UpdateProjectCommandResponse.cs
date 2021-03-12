94be55bb7044b22a00d7a62b3846858523acb4c8
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-12 23:14:21 +0100

    created update project command response

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Commands/UpdateProject/UpdateProjectCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+
+namespace ProjectManagement.Core.UseCases.Projects.Commands.UpdateProject
+{
+    public class UpdateProjectCommandResponse : BaseResponse
+    {
+        public int? ProjectId { get; set; }
+
+        public UpdateProjectCommandResponse() : base() { }
+        public UpdateProjectCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public UpdateProjectCommandResponse(string message) : base(message) { }
+        public UpdateProjectCommandResponse(string message, bool success) : base(message, success) { }
+        public UpdateProjectCommandResponse(int projectId) => ProjectId = projectId;
+
+    }
+}
