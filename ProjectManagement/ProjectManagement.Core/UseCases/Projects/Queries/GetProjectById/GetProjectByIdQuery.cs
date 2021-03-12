2e9f4934825ab9fd12afa2be6c32445438fd2074
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-12 20:58:56 +0100

    created get project by id query

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Queries/GetProjectById/GetProjectByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using ProjectManagement.Core.UseCases.Projects.Queries.GetProjectById.Dto;
+
+namespace ProjectManagement.Core.UseCases.Projects.Queries.GetProjectById
+{
+    public class GetProjectByIdQuery : IRequest<DetailedProjectDto>
+    {
+        public int ProjectId { get; set; }
+    }
+}
