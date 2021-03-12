801a88f283dd55cc4e77659c6eab6d417c6b29c0
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-12 21:00:14 +0100

    removed description and customer id from project dto

--- ProjectManagement/ProjectManagement.Core/UseCases/Projects/Queries/GetProjects/Dto/ProjectDto.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Queries/GetProjects/Dto/ProjectDto.cs
@@ -10,12 +10,10 @@ namespace ProjectManagement.Core.UseCases.Projects.Queries.GetProjects.Dto
         [JsonProperty(Order = int.MinValue)]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime TargetDate { get; set; }
         public DateTime EndDate { get; set; }
         public bool IsActive { get; set; }
-        public int CustomerId { get; set; }
 
         public static void Mapping(MappingProfile profile)
         {
