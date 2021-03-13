a1d628e509a8af61ba7aca3793d4abc51ee9fbec
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-13 12:35:04 +0100

    created customer dto

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Customers/Dto/CustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.Customers.Dto
+{
+    public class CustomerDto : IMapFrom<Customer>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Country { get; set; }
+
+        public static void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<Customer, CustomerDto>();
+        }
+    }
+}
