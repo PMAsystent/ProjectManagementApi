8735b33dbe3375f1f11dcbd5b6d9dfdbfd6aa445
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-13 12:35:59 +0100

    implemented get customers query in customer controller

---
+++ ProjectManagement/ProjectManagementApi/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Customers.Queries.GetCustomers;
+using ProjectManagement.Core.UseCases.Customers.ViewModels;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApi.Controllers
+{
+    public class CustomersController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<CustomerVm>> GetAllCustomers()
+        {
+            return await Mediator.Send(new GetCustomersQuery());
+        }
+    }
+}
