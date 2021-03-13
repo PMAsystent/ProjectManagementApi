c9b5c3d6d2b5daf92a7d22e5221b6166983ce012
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-13 13:13:01 +0100

    implemented get customer by id query in customer controller

--- ProjectManagement/ProjectManagementApi/Controllers/CustomersController.cs
+++ ProjectManagement/ProjectManagementApi/Controllers/CustomersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Customers.Dto;
+using ProjectManagement.Core.UseCases.Customers.Queries.GetCustomerById;
 using ProjectManagement.Core.UseCases.Customers.Queries.GetCustomers;
 using ProjectManagement.Core.UseCases.Customers.ViewModels;
 using System.Threading.Tasks;
@@ -11,6 +13,16 @@ namespace ProjectManagementApi.Controllers
         public async Task<ActionResult<CustomerVm>> GetAllCustomers()
         {
             return await Mediator.Send(new GetCustomersQuery());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DetailedCustomerDto>> GetCustomerById(int id)
+        {
+            var getCustomerQuery = new GetCustomerByIdQuery()
+            {
+                CustomerId = id
+            };
+            return await Mediator.Send(getCustomerQuery);
         }
     }
 }
