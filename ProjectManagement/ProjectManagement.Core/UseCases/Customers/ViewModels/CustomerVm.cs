eede45b9c93d882e7f9a201037f8a66f7c29d5db
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-13 12:35:13 +0100

    created customer vm

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Customers/ViewModels/CustomerVm.cs
@@ -0,0 +1,11 @@
+﻿using ProjectManagement.Core.UseCases.Customers.Dto;
+using System.Collections.Generic;
+
+namespace ProjectManagement.Core.UseCases.Customers.ViewModels
+{
+    public class CustomerVm
+    {
+        public IList<CustomerDto> Customers { get; set; }
+        public int Count { get; set; }
+    }
+}
