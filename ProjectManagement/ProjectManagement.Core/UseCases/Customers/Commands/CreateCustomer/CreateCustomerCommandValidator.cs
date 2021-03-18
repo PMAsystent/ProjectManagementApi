5e8a69135966ba1103aa7aad23c6473c9c686e0f
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-18 23:05:35 +0100

    created create customer command validator

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Customers/Commands/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace ProjectManagement.Core.UseCases.Customers.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MaximumLength(20);
+
+            RuleFor(c => c.Surname).NotEmpty();
+            RuleFor(c => c.Name).MaximumLength(30);
+
+            RuleFor(c => c.Address).NotEmpty();
+
+            RuleFor(c => c.Country).NotEmpty();
+
+            RuleFor(c => c.PostCode).NotEmpty();
+
+            //Phone number validation
+            //RuleFor(c => c.PhoneNumber)
+            RuleFor(c => c.Email).EmailAddress();
+        }
+    }
+}
