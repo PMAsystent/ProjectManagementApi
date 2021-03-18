86152763a99eb261c41a6be4b199afeb0fda57b1
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-18 22:57:55 +0100

    created create customer command response

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Customers/Commands/CreateCustomer/CreateCustomerCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+
+namespace ProjectManagement.Core.UseCases.Customers.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandResponse : BaseResponse
+    {
+        public int? CustomerId { get; set; }
+
+        public CreateCustomerCommandResponse() : base() { }
+        public CreateCustomerCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public CreateCustomerCommandResponse(string message) : base(message) { }
+        public CreateCustomerCommandResponse(string message, bool success) : base(message, success) { }
+        public CreateCustomerCommandResponse(int customerId) => CustomerId = customerId;
+    }
+}
