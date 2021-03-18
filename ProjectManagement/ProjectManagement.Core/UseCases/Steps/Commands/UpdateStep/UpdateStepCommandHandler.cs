ed61dc170b426ff7d6ba997ad6b39cebaea06ab4
Konrad <konradlubera@hotmail.com>
2021-03-18 21:07:17 +0100

    Added update step commands

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Steps/Commands/UpdateStep/UpdateStepCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Interfaces;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.Steps.Commands.UpdateStep
+{
+    public class UpdateStepCommandHandler : IRequestHandler<UpdateStepCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UpdateStepCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateStepCommand request, CancellationToken cancellationToken)
+        {
+            
+            var step = _mapper.Map<Step>(request);
+            _context.Steps.Update(step);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
