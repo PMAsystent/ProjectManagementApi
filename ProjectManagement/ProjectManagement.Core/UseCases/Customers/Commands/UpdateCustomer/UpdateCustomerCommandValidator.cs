d868eabed67f35ce7c3fb02ef479720d04606111
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-19 16:34:02 +0100

    created update customer command validator

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace ProjectManagement.Core.UseCases.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+    {
+        public UpdateCustomerCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MaximumLength(20);
+
+            RuleFor(c => c.Surname).NotEmpty();
+            RuleFor(c => c.Name).MaximumLength(30);
+
+            RuleFor(c => c.Address).NotEmpty();
+
+            RuleFor(c => c.Country).NotEmpty();
+
+            RuleFor(c => c.PostCode).NotEmpty();
+
+            //Phone number validation
+            //RuleFor(c => c.PhoneNumber)
+            RuleFor(c => c.Email).EmailAddress();
+        }
+    }
+}
