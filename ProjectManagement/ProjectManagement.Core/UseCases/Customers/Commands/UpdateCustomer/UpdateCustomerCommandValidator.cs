870f30bfedbfda61c49ceb5def0792e52b9051fd
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-23 00:18:01 +0100

    refactored UpdateCustomerCommandValidator

--- ProjectManagement/ProjectManagement.Core/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommandValidator.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -6,21 +6,25 @@ namespace ProjectManagement.Core.UseCases.Customers.Commands.UpdateCustomer
     {
         public UpdateCustomerCommandValidator()
         {
+            RuleFor(c => c.Id)
+                .GreaterThan(0)
+                .WithMessage("Id must be more than zero");
+
+            // TODO: Think about solution with avoiding code repeating (create project command validator & this)
+
             RuleFor(c => c.Name).NotEmpty();
-            RuleFor(c => c.Name).MaximumLength(20);
 
             RuleFor(c => c.Surname).NotEmpty();
-            RuleFor(c => c.Name).MaximumLength(30);
+
+            RuleFor(c => c.Email).EmailAddress();
+
+            RuleFor(c => c.PhoneNumber).NotEmpty();
 
             RuleFor(c => c.Address).NotEmpty();
 
-            RuleFor(c => c.Country).NotEmpty();
-
             RuleFor(c => c.PostCode).NotEmpty();
 
-            //Phone number validation
-            //RuleFor(c => c.PhoneNumber)
-            RuleFor(c => c.Email).EmailAddress();
+            RuleFor(c => c.Country).NotEmpty();
         }
     }
 }
