c2d0774774679ee25157cbb471ef7edf5d80a3d8
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-22 23:38:08 +0100

    added description to project dto

--- ProjectManagement/ProjectManagement.Core/UseCases/Projects/Dto/ProjectDto.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Dto/ProjectDto.cs
@@ -10,6 +10,7 @@ namespace ProjectManagement.Core.UseCases.Projects.Dto
         [JsonProperty(Order = int.MinValue)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime TargetDate { get; set; }
         public DateTime EndDate { get; set; }
