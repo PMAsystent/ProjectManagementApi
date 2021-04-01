fa08f4821deac00e8c61dfd3745279b68caa1fb2
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-01 17:39:43 +0200

    created boss dto

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Bosses/Dto/BossDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.Bosses.Dto
+{
+    public class BossDto : IMapFrom<Boss>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public static void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<Boss, BossDto>();
+        }
+    }
+}
