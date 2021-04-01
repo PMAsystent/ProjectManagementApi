3de24492ae9eb5310454ec17f9d8dd38873a48c8
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-01 17:40:20 +0200

    created get bosses query

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Bosses/Commands/GetBosses/GetBossesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProjectManagement.Core.UseCases.Bosses.ViewModels;
+
+namespace ProjectManagement.Core.UseCases.Bosses.Commands.GetBosses
+{
+    public class GetBossesQuery : IRequest<BossVm>
+    {
+    }
+}
