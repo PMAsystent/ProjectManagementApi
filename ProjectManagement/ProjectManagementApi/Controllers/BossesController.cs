f83810171c8702c43381d1b27793574ef632b356
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-01 17:47:36 +0200

    created bosses controller and implemented getting all bosses there

---
+++ ProjectManagement/ProjectManagementApi/Controllers/BossesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Bosses.Commands.GetBosses;
+using ProjectManagement.Core.UseCases.Bosses.ViewModels;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApi.Controllers
+{
+    public class BossesController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<BossVm>> GetAllBosses()
+        {
+            return await Mediator.Send(new GetBossesQuery());
+        }
+    }
+}
