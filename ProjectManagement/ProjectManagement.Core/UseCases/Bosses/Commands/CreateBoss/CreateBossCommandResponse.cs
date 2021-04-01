7297fcc612c451a29ffe4dda4bd2145923d5a6a3
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-01 18:27:33 +0200

    created create boss command response

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Bosses/Commands/CreateBoss/CreateBossCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+using ProjectManagement.Core.UseCases.Bosses.Dto;
+
+namespace ProjectManagement.Core.UseCases.Bosses.Commands.CreateBoss
+{
+    public class CreateBossCommandResponse : BaseResponse
+    {
+        public DetailedBossDto DetailedBossDto { get; set; }
+
+        public CreateBossCommandResponse() : base() { }
+        public CreateBossCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public CreateBossCommandResponse(string message) : base(message) { }
+        public CreateBossCommandResponse(string message, bool success) : base(message, success) { }
+        public CreateBossCommandResponse(DetailedBossDto detailedCustomerDto) => DetailedBossDto = detailedCustomerDto;
+    }
+}
