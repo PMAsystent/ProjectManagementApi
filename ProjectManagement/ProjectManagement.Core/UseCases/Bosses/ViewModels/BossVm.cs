be4e2782a4889c4a1af02b01a663445cb203886d
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-01 17:39:56 +0200

    created boss vm

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Bosses/ViewModels/BossVm.cs
@@ -0,0 +1,11 @@
+﻿using ProjectManagement.Core.UseCases.Bosses.Dto;
+using System.Collections.Generic;
+
+namespace ProjectManagement.Core.UseCases.Bosses.ViewModels
+{
+    public class BossVm
+    {
+        public IList<BossDto> Bosses { get; set; }
+        public int Count { get; set; }
+    }
+}
