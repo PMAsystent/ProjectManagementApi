4eb6409e84b1c976b3a02bd0658ca9328309e837
Konrad <konradlubera@hotmail.com>
2021-04-16 22:16:04 +0200

    Created CreatTaskCommandResponse

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Tasks/Commands/CreateTask/CreateTaskCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+
+namespace ProjectManagement.Core.UseCases.Tasks.Commands.CreateTask
+{
+    public class CreateTaskCommandResponse : BaseResponse
+    {
+        public Task Task { get; set; }
+        
+        public CreateTaskCommandResponse() : base() {}
+        public CreateTaskCommandResponse(ValidationResult validationResult) : base(validationResult) {}
+        public CreateTaskCommandResponse(string message) : base(message){}
+        public CreateTaskCommandResponse(string message, bool success) : base(message, success) {}
+        public CreateTaskCommandResponse(Task task) => Task = task;
+    }
+}
