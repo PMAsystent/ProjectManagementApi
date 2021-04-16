b3e737ec46f4ec5db66dab74b52e452b7b931910
Konrad <konradlubera@hotmail.com>
2021-04-16 22:16:35 +0200

    Created TasksController

---
+++ ProjectManagement/ProjectManagementApi/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Tasks.Commands.CreateTask;
+using Task = Domain.Entities.Task;
+
+namespace ProjectManagementApi.Controllers
+{
+    public class TasksController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<Task>> AddTask([FromBody] CreateTaskCommand createTaskCommand)
+        {
+            var result = await Mediator.Send(createTaskCommand); 
+            return Ok(result.Task);
+        }
+    }
+}
