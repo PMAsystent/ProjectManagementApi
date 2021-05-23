99bb5c88cda8a310521ea1664f3671d9a45c9e02
Konrad <konradlubera@hotmail.com>
2021-05-23 23:33:06 +0200

    Created delete command handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Tasks/Commands/DeleteTask/DeleteTaskCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ProjectManagement.Core.Base.Exceptions;
+using ProjectManagement.Core.Base.Interfaces;
+
+namespace ProjectManagement.Core.UseCases.Tasks.Commands.DeleteTask
+{
+    public class DeleteTaskCommandHandler : IRequestHandler<DeleteTaskCommand>
+    {
+
+        private readonly IApplicationDbContext _context;
+
+        public DeleteTaskCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Unit> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+        {
+            var task = await _context.Tasks.FindAsync(request.TaskId);
+            if (task == null)
+            {
+                throw new NotFoundException(nameof(Task), request.TaskId);
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
