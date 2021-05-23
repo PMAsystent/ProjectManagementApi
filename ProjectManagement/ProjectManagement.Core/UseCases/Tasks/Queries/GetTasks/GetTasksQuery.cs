9128c6569c7c6838e94a31134130eb7ad0e78a84
Konrad <konradlubera@hotmail.com>
2021-05-23 23:37:35 +0200

    Created get task query

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Tasks/Queries/GetTasks/GetTasksQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using ProjectManagement.Core.UseCases.Tasks.ViewModels;
+
+namespace ProjectManagement.Core.UseCases.Tasks.Queries.GetTasks
+{
+    public class GetTasksQuery : IRequest<TaskVM> { }
+}
