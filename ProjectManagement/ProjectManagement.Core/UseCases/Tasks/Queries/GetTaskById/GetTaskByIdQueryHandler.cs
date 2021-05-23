bbcf10d1002adb7a0bf7238f134f3ad527169d59
Konrad <konradlubera@hotmail.com>
2021-05-23 23:38:41 +0200

    Created get task by id query handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Tasks/Queries/GetTaskById/GetTaskByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using ProjectManagement.Core.Base.Exceptions;
+using ProjectManagement.Core.Base.Interfaces;
+using ProjectManagement.Core.UseCases.Tasks.Dto;
+
+namespace ProjectManagement.Core.UseCases.Tasks.Queries.GetTaskById
+{
+    public class GetTaskByIdQueryHandler : IRequestHandler<GetTaskByIdQuery, TaskDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetTaskByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<TaskDto> Handle(GetTaskByIdQuery request, CancellationToken cancellationToken)
+        {
+            var task = await _context.Tasks.FindAsync(request.TaskId);
+            if (task == null)
+            {
+                throw new NotFoundException(nameof(Task), request.TaskId);
+            }
+
+            var taskToReturn = _mapper.Map<TaskDto>(task);
+
+            return taskToReturn;
+        }
+    }
+}
