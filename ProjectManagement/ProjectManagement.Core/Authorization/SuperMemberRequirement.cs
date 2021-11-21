7d053811e0601b56643a31c87a02e7337de36679
Konrad <konradlubera@hotmail.com>
2021-11-21 21:10:54 +0100

    Edited requirements to handle step id and task id

--- ProjectManagement/ProjectManagement.Core/Authorization/SuperMemberRequirement.cs
+++ ProjectManagement/ProjectManagement.Core/Authorization/SuperMemberRequirement.cs
@@ -1,8 +1,12 @@
-﻿using System.Threading;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Domain.Entities;
 using Domain.Enums;
 using MediatR.Behaviors.Authorization;
 using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Core.Base.Exceptions;
 using ProjectManagement.Core.Base.Interfaces;
 
 namespace ProjectManagement.Core.Authorization
@@ -10,7 +14,9 @@ namespace ProjectManagement.Core.Authorization
     public class SuperMemberRequirement : IAuthorizationRequirement
     {
         public string UserId { get; set; }
-        public int ProjectId { get; set; }
+        public int? ProjectId { get; set; }
+        public int? StepId { get; set; }
+        public int? TaskId { get; set; }
 
         class SuperMemberRequirementHandler : IAuthorizationHandler<SuperMemberRequirement>
         {
@@ -25,6 +31,32 @@ namespace ProjectManagement.Core.Authorization
             {
                 var userId = requirement.UserId;
                 var projectId = requirement.ProjectId;
+                
+                if (projectId == null)
+                {
+                    var step = new Step();
+                    
+                    if (requirement.StepId != null)
+                    {
+                        step = await _context.Steps.FindAsync(requirement.StepId);
+                    }
+                    else if (requirement.TaskId != null)
+                    {
+                        step = await _context.Steps.FirstOrDefaultAsync(s =>
+                            s.Tasks.FirstOrDefault(t => t.Id == requirement.TaskId) != null, cancellationToken);
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                    
+                    if (step == null)
+                    {
+                        return AuthorizationResult.Fail("Action not allowed");
+                    }
+                    
+                    projectId = step.ProjectId;
+                }
                 
                 var assignment = await _context.ProjectAssignments
                     .FirstOrDefaultAsync(a => 
