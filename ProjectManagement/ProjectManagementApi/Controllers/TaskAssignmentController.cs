8f07ccc55fa8cd18b9e65194004d5c435003c98b
Konrad <konradlubera@hotmail.com>
2021-11-22 23:07:59 +0100

    Added task assigment controller

---
+++ ProjectManagement/ProjectManagementApi/Controllers/TaskAssignmentController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR.Behaviors.Authorization.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.TaskAssignments.Commands.AssignUserToTask;
+using ProjectManagement.Core.UseCases.TaskAssignments.Commands.UnassignUserFromTask;
+using ProjectManagement.Core.UseCases.TaskAssignments.Queries.GetUnassignedToTaskUsers;
+
+namespace ProjectManagementApi.Controllers
+{
+    public class TaskAssignmentController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> AssignUserToTask([FromBody] AssignUserToTaskCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+                return Ok();
+            }
+            catch (UnauthorizedException e)
+            {
+                return StatusCode(401, e.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        
+        [HttpDelete]
+        public async Task<ActionResult> UnassignUserFromTask([FromBody] UnassignUserFromTaskCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+                return Ok();
+            }
+            catch (UnauthorizedException e)
+            {
+                return StatusCode(401, e.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        
+        [HttpGet("unassignedUsers/{taskId}")]
+        public async Task<ActionResult> GetUnassignedUsers(int taskId)
+        {
+            try
+            {
+                var query = new GetUnassignedToTaskUserQuery
+                {
+                    TaskId = taskId
+                };
+                
+                var result = await Mediator.Send(query);
+                return Ok(result);
+            }
+            catch (UnauthorizedException e)
+            {
+                return StatusCode(401, e.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
