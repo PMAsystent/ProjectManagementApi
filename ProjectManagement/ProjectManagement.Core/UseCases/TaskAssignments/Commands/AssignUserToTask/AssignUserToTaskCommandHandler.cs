cd8b255932dcefe9a6d7d270739ff2818628b2d1
Konrad <konradlubera@hotmail.com>
2021-11-22 23:07:08 +0100

    Added assign to task command

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/TaskAssignments/Commands/AssignUserToTask/AssignUserToTaskCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Interfaces;
+
+namespace ProjectManagement.Core.UseCases.TaskAssignments.Commands.AssignUserToTask
+{
+    public class AssignUserToTaskCommandHandler : IRequestHandler<AssignUserToTaskCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AssignUserToTaskCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<Unit> Handle(AssignUserToTaskCommand request, CancellationToken cancellationToken)
+        {
+            var taskAssigment = _mapper.Map<TaskAssignment>(request);
+            taskAssigment.isActive = true;
+            await _context.TaskAssignments.AddAsync(taskAssigment, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
