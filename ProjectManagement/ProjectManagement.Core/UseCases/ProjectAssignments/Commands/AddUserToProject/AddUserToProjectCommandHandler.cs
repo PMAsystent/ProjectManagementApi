24c3576885b25a922580520e6325f23d80a2c3fd
Konrad <konradlubera@hotmail.com>
2021-11-25 19:48:20 +0100

    Added enum validation

--- ProjectManagement/ProjectManagement.Core/UseCases/ProjectAssignments/Commands/AddUserToProject/AddUserToProjectCommandHandler.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectAssignments/Commands/AddUserToProject/AddUserToProjectCommandHandler.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using Domain.Entities;
+using Domain.Enums;
 using MediatR;
 using ProjectManagement.Core.Base.Interfaces;
 
@@ -20,6 +22,18 @@ namespace ProjectManagement.Core.UseCases.ProjectAssignments.Commands.AddUserToP
         
         public async Task<Unit> Handle(AddUserToProjectCommand request, CancellationToken cancellationToken)
         {
+            ProjectRole role;
+            ProjectMemberType memberType;
+            
+            if (Enum.TryParse(request.ProjectRole, out role))
+            {
+                throw new ArgumentException("Wrong project role");
+            }
+            if (Enum.TryParse(request.MemberType, out memberType))
+            {
+                throw new ArgumentException("Wrong member type");
+            }
+            
             var projectAssignment = _mapper.Map<ProjectAssignment>(request);
             await _context.ProjectAssignments.AddAsync(projectAssignment, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
