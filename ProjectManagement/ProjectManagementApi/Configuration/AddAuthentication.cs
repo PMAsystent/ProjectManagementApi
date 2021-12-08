fd6e2e79cef2801ed99e93cc633d1aab3c25f746
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-12-08 20:04:49 +0100

    refactored

--- ProjectManagement/ProjectManagementApi/Configuration/AddAuthentication.cs
+++ ProjectManagement/ProjectManagementApi/Configuration/AddAuthentication.cs
@@ -14,42 +14,52 @@ namespace ProjectManagementApi.Configuration
         {
             if (configuration.GetValue<bool>("Testing"))
             {
-                services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = "Integration tests";
-                    options.DefaultChallengeScheme = "Integration tests";
-                }).AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>(
-                    "Integration tests", options => { });
+                AddTestingAuthentication(services);
             }
             else
             {
-                var settingsSection = configuration.GetSection("AppSettings");
-                var settings = settingsSection.Get<AppSettings>();
-                services.Configure<AppSettings>(settingsSection);
-
-                services.AddAuthentication(x =>
-                    {
-                        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    })
-                    .AddJwtBearer(options =>
-                    {
-                        // IdentityServer emits a typ header by default, recommended extra check
-                        options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
-
-                        //SET ONLY IN-DEV TODO: make this automatic
-                        options.RequireHttpsMetadata = false;
-
-                        options.SaveToken = true;
-                        options.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ValidateIssuerSigningKey = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.AuthKey)),
-                            ValidateIssuer = false,
-                            ValidateAudience = false,
-                        };
-                    });
+                AddApplicationAuthentication(services, configuration);
             }
+        }
+
+        private static void AddTestingAuthentication(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "Integration tests";
+                options.DefaultChallengeScheme = "Integration tests";
+            }).AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>(
+                "Integration tests", options => { });
+        }
+
+        private static void AddApplicationAuthentication(IServiceCollection services, IConfiguration configuration)
+        {
+            var settingsSection = configuration.GetSection("AppSettings");
+            var settings = settingsSection.Get<AppSettings>();
+            services.Configure<AppSettings>(settingsSection);
+
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    // IdentityServer emits a typ header by default, recommended extra check
+                    options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+
+                    //SET ONLY IN-DEV TODO: make this automatic
+                    options.RequireHttpsMetadata = false;
+
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.AuthKey)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
         }
     }
 }
