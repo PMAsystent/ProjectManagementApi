02501a5c43ec7bfd1bd8f284c2054a1d82fb0ad0
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-12-21 21:17:37 +0100

    unit tests

---
+++ ProjectManagement/tests/ProjectManagement.Core.UnitTests/IProjectPercentageService/GetProgressPercentageForProject.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+using Domain.Enums;
+using FluentAssertions;
+using ProjectManagement.Core.Base.Utils;
+using Xunit;
+
+namespace ProjectManagement.Core.UnitTests.IProjectPercentageService
+{
+    public class GetProgressPercentageForProject
+    {
+        private readonly IProgressPercentageService _service = new ProgressPercentageService();
+        
+        [Fact]
+        public void With1Step2Tasks()
+        {
+            const int correctValue = 50;
+
+            var project = new Project()
+            {
+                Steps = new List<Step>()
+                {
+                    new()
+                    {
+                        Tasks = new List<Task>()
+                        {
+                            new() { TaskStatus = TaskStatus.Done.ToString() },
+                            new() { TaskStatus = TaskStatus.InProgress.ToString() }
+                        }
+                    },
+                }
+            };
+
+            var calculatedPercent = _service.GetProgressPercentageForProject(project.Steps.ToList());
+            
+            calculatedPercent.Should().Be(correctValue);
+        }
+        
+        [Fact]
+        public void With2Step4Tasks()
+        {
+            const int correctValue = 50;
+
+            var project = new Project()
+            {
+                Steps = new List<Step>()
+                {
+                    new()
+                    {
+                        Tasks = new List<Task>()
+                        {
+                            new() { TaskStatus = TaskStatus.Done.ToString() },
+                            new() { TaskStatus = TaskStatus.InProgress.ToString() }
+                        }
+                    },
+                    new()
+                    {
+                        Tasks = new List<Task>()
+                        {
+                            new() { TaskStatus = TaskStatus.Done.ToString() },
+                            new() { TaskStatus = TaskStatus.InProgress.ToString() }
+                        }
+                    },
+                }
+            };
+
+            var calculatedPercent = _service.GetProgressPercentageForProject(project.Steps.ToList());
+            
+            calculatedPercent.Should().Be(correctValue);
+        }
+        
+        [Fact]
+        public void WithoutTasks()
+        {
+            const int correctValue = 0;
+
+            var project = new Project()
+            {
+                Steps = new List<Step>()
+                {
+                    new()
+                    {
+                        Tasks = new List<Task>()
+                        {
+                        }
+                    }
+                }
+            };
+
+            var calculatedPercent = _service.GetProgressPercentageForProject(project.Steps.ToList());
+            
+            calculatedPercent.Should().Be(correctValue);
+        }
+
+    }
+}
