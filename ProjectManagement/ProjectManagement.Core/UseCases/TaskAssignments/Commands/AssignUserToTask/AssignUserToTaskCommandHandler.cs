a874b5f3621ab85969ff5d761760e1a8b081f646
Konrad Lubera <konradlubera@hotmail.com>
2021-12-29 21:46:19 +0100

    Added missing validations

--- ProjectManagement/ProjectManagement.Core/UseCases/TaskAssignments/Commands/AssignUserToTask/AssignUserToTaskCommandHandler.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/TaskAssignments/Commands/AssignUserToTask/AssignUserToTaskCommandHandler.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Core.Base.Exceptions;
 using ProjectManagement.Core.Base.Interfaces;
+using Task = Domain.Entities.Task;
 
 namespace ProjectManagement.Core.UseCases.TaskAssignments.Commands.AssignUserToTask
 {
@@ -20,12 +24,56 @@ namespace ProjectManagement.Core.UseCases.TaskAssignments.Commands.AssignUserToT
         
         public async Task<Unit> Handle(AssignUserToTaskCommand request, CancellationToken cancellationToken)
         {
+            if (!await TaskExists(request.TaskId))
+            {
+                throw new NotFoundException(nameof(Task), request.TaskId);
+            }
+
+            if (!await UserAssignedToProject(request.UserId, request.TaskId))
+            {
+                throw new Exception("User is not assigned to project.");
+            }
+
+            if (await UserAlreadyAssigned(request.UserId, request.TaskId))
+            {
+                throw new Exception("User is already assigned to task");
+            }
+            
             var taskAssigment = _mapper.Map<TaskAssignment>(request);
             taskAssigment.isActive = true;
             await _context.TaskAssignments.AddAsync(taskAssigment, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
             
             return Unit.Value;
+        }
+
+        private async Task<bool> UserAlreadyAssigned(int userId, int taskId)
+        {
+            var taskAssignment = await _context.TaskAssignments
+                .FirstOrDefaultAsync(a => a.UserId == userId && a.TaskId == taskId && a.isActive);
+            
+            return taskAssignment != null;
+        }
+        
+        private async Task<bool> UserAssignedToProject(int userId, int taskId)
+        {
+            var projectId = await GetProjectId(taskId);
+            var projectAssigment = await _context.ProjectAssignments
+                .FirstOrDefaultAsync(a => a.UserId == userId && a.ProjectId == projectId);
+            
+            return projectAssigment != null;
+        }
+        
+        private async Task<bool> TaskExists(int taskId) => await _context.Tasks.FindAsync(taskId) != null;
+        
+        private async Task<int> GetProjectId(int taskId)
+        {
+            var task = await _context.Tasks
+                .Include(t => t.Step)
+                .Include(t => t.Step.Project)
+                .FirstOrDefaultAsync(t => t.Id == taskId);
+
+            return task.Step.Project.Id;
         }
     }
 }
