4a3c46b4577d26b6aab127770208cdac3676ae09
Konrad Lubera <konradlubera@hotmail.com>
2021-12-29 22:14:13 +0100

    Added project creator and task description to detailed project dto

--- ProjectManagement/ProjectManagement.Core/UseCases/Projects/Dto/ProjectTaskDto.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Projects/Dto/ProjectTaskDto.cs
@@ -8,6 +8,7 @@ namespace ProjectManagement.Core.UseCases.Projects.Dto
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Priority { get; set; }
         public string TaskStatus { get; set; }
         public DateTime DueDate { get; set; }
