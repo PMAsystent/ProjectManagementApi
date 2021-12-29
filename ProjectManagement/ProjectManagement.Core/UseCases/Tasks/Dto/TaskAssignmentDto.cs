7ccbe168fcca4f7b58b9e5a5e069fc822724af65
Konrad Lubera <konradlubera@hotmail.com>
2021-12-29 21:42:07 +0100

    Return assigned users with detailed task dto

--- ProjectManagement/ProjectManagement.Core/UseCases/Tasks/Dto/TaskAssignmentDto.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Tasks/Dto/TaskAssignmentDto.cs
@@ -3,15 +3,14 @@ using ProjectManagement.Core.Base.Mappings;
 
 namespace ProjectManagement.Core.UseCases.Tasks.Dto
 {
-    public class TaskAssignemntDto : IMapFrom<TaskAssignment>
+    public class TaskAssignedUserDto : IMapFrom<User>
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int TaskId { get; set; }
+        public string UserName { get; set; }
         
         public static void Mapping(MappingProfile profile)
         {
-            profile.CreateMap<TaskAssignemntDto, TaskAssignment>().ReverseMap();
+            profile.CreateMap<TaskAssignedUserDto, User>().ReverseMap();
         }
     }
 }
