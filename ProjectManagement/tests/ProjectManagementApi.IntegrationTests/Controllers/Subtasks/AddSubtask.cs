2c2e1d64eeae3dbc5d14678c9c20f8ef8861f7a6
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-01-13 13:41:19 +0100

    implement tests

---
+++ ProjectManagement/tests/ProjectManagementApi.IntegrationTests/Controllers/Subtasks/AddSubtask.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using ProjectManagement.Core.UseCases.Subtasks.Commands.CreateSubtask;
+using ProjectManagementApi.IntegrationTests.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProjectManagementApi.IntegrationTests.Controllers.Subtasks
+{
+    [Collection("Sequential")]
+    public class AddSubtask:IntegrationTest
+    {
+        public AddSubtask(CustomWebApplicationFactory<Startup> factory, ITestOutputHelper output) : base(factory, output)
+        {
+        }
+
+        [Fact]
+        public async Task AddSubtaskSuccessful()
+        {
+            var request = new CreateSubtaskCommand()
+            {
+                Name = "new subtask",
+                TaskId = Utilities.P1S1Task1.Id
+            };
+            
+            var response = await Client.PostAsync("api/Subtasks",Utilities.GetRequestContent(request));
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
