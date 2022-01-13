71a1bbf6a325bbc66fc7f1e43bc89cb515f86532
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-01-13 14:39:18 +0100

    implement tasks tests and put back property is done into create subtask command (#71)

--- ProjectManagement/ProjectManagement.Core/UseCases/Subtasks/Commands/CreateSubtask/CreateSubtaskCommand.cs
+++ ProjectManagement/ProjectManagement.Core/UseCases/Subtasks/Commands/CreateSubtask/CreateSubtaskCommand.cs
@@ -7,8 +7,9 @@ namespace ProjectManagement.Core.UseCases.Subtasks.Commands.CreateSubtask
     public class CreateSubtaskCommand : IRequest<CreateSubtaskCommandResponse>, IMapFrom<Subtask>
     {
         public string Name { get; set; }
+        public bool IsDone { get; set; }
         public int TaskId { get; set; }
-        
+
         public void Mapping(MappingProfile profile)
         {
             profile.CreateMap<CreateSubtaskCommand, Subtask>();
