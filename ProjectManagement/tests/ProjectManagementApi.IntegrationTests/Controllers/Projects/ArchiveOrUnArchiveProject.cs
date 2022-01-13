e5fe87be61fdf9af1badd44d08a1aaa8500c0537
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-01-13 13:12:29 +0100

    integration_tests/projects (#67)
    * implemented tests for projects
    
    * reverted connection string

---
+++ ProjectManagement/tests/ProjectManagementApi.IntegrationTests/Controllers/Projects/ArchiveOrUnArchiveProject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ProjectManagementApi.IntegrationTests.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProjectManagementApi.IntegrationTests.Controllers.Projects
+{
+    [Collection("Sequential")]
+    public class ArchiveOrUnArchiveProject : IntegrationTest
+    {
+        public ArchiveOrUnArchiveProject(CustomWebApplicationFactory<Startup> factory, ITestOutputHelper output) : base(factory, output)
+        {
+        }
+
+        [Fact]
+        public async Task ArchiveOrUnArchiveProjectSuccessful()
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Patch,
+                RequestUri = new Uri($"/api/MyProjects/{Utilities.Project1.Id}/archive/{true}", UriKind.Relative)
+            };
+            
+            var response = await Client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+        
+        // [Fact]
+        // public async Task ArchiveOrUnArchiveProjectWhenUnauthorized()
+        // {
+        //     throw new NotImplementedException();
+        // }
+    }
+}
---
+++ ProjectManagement/tests/ProjectManagementApi.IntegrationTests/Controllers/Projects/UpdateProject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ProjectManagement.Core.UseCases.Projects.Commands.UpdateProject;
+using ProjectManagement.Core.UseCases.Projects.Dto;
+using ProjectManagementApi.IntegrationTests.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProjectManagementApi.IntegrationTests.Controllers.Projects
+{
+    [Collection("Sequential")]
+    public class UpdateProject : IntegrationTest
+    {
+        public UpdateProject(CustomWebApplicationFactory<Startup> factory, ITestOutputHelper output) : base(factory,
+            output)
+        {
+        }
+
+
+        [Fact]
+        public async Task UpdateProjectSuccessful()
+        {
+            var newName = "new name";
+            var newDescription = "new description";
+            var newDate = DateTime.UtcNow.AddDays(20);
+
+            var request = new UpdateProjectCommand()
+            {
+                Id = Utilities.Project1.Id,
+                Name = newName,
+                Description = newDescription,
+                DueDate = newDate,
+            };
+
+            var response = await Client.PutAsync("/api/MyProjects", Utilities.GetRequestContent(request));
+            response.EnsureSuccessStatusCode();
+
+            var getResponse = await Client.GetAsync($"/api/MyProjects/{Utilities.Project1.Id}");
+            var projectAfterUpdate = await Utilities.GetResponseContent<DetailedProjectDto>(getResponse);
+            projectAfterUpdate.Name.Should().Be(newName);
+            projectAfterUpdate.Description.Should().Be(newDescription);
+            projectAfterUpdate.DueDate.Should().Be(newDate);
+        }
+
+        // [Fact]
+        // public async Task UpdateProjectWhenUnauthorized()
+        // {
+        //     throw new NotImplementedException();
+        // }
+    }
+}
