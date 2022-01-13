9d54f6693c6537a959fccac81b27dcd5b1ea3cd4
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-01-13 14:35:34 +0100

    implement tasks tests and put back property is done into create subtask command

---
+++ ProjectManagement/tests/ProjectManagementApi.IntegrationTests/Controllers/Tasks/DeleteTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using ProjectManagementApi.IntegrationTests.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProjectManagementApi.IntegrationTests.Controllers.Tasks
+{
+    [Collection("Sequential")]
+    public class DeleteTask : IntegrationTest
+    {
+        public DeleteTask(CustomWebApplicationFactory<Startup> factory, ITestOutputHelper output) : base(factory, output)
+        {
+        }
+        
+        [Fact]
+        public async Task DeleteTaskSuccessful()
+        {
+            var response = await Client.DeleteAsync($"api/Tasks/{Utilities.P1S1Task1.Id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
